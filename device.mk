b27e9ab3b63fab149ff7be140e08537f6d3f3431
Michael Wright <michaelwr@google.com>
2017-01-31 18:34:15 +0000

    Custom Vibrator implementation
    Add support for new amplitude controls.
    
    Test: Manual testing with vendor/google_experimental/users/michaelwr/Vibrator
          & adb shell /data/nativetest/vibrator_hidl_hal_test/vibrator_hidl_hal_test
    Change-Id: I7e8c489169d2955d3c5fcc7e6aa0e9aa12bf88a2
    
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -361,8 +361,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator HAL
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.marlin
 
 PRODUCT_PACKAGES += \
     vndk-sp
