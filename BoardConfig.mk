444aadcd459819308124ee6d383fb75466fc3445
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-10-09 05:05:58 -0500

    addison: add QCNE Peripheralmanager and enable qcom sepolicy
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    Signed-off-by: klozz <klozz707@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,9 @@ BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
+#CNE/DPM
+BOARD_USES_QCNE := true
+
 # Display
 BOARD_USES_ADRENO := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
@@ -149,7 +152,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
