4811ea635eda60342fa17e5d5a3d1c091ec5b25c
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-10-12 19:22:14 -0500

    addison: load the greybus driver when it is starting.
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -53,6 +53,15 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.gbmods.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.gbmods.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mmi.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
---
+++ rootdir/etc/init.gbmods.sh
@@ -0,0 +1,38 @@
+#!/system/bin/sh
+# Original author: Motorola Mobility LLC
+# Modified author: Klozz Jesús (TeamMEX@XDA-Developers)
+# Please don't remove the author file i/e modifique the same
+# Thanks to Davtse for the help doing this work.
+# date: 12 oct 2017 19:21:09
+
+debug=$(getprop ro.boot.gbdebug 2> /dev/null)
+bootmode=$(getprop ro.bootmode 2> /dev/null)
+
+# If androidboot.gbdebug is set on command line, skip inserting
+# the pre-installed modules.
+if [ "$debug" == "1" ]; then
+    return 0
+fi
+
+#Load the Greybus main driver 
+insmod /system/lib/modules/greybus.ko
+
+# Only support PTP and BATTERY in charge-only mode
+if [ "$bootmode" == "charger" ]; then
+    insmod /system/lib/modules/gb-mods.ko
+    insmod /system/lib/modules/gb-battery.ko
+    insmod /system/lib/modules/gb-ptp.ko
+
+    return 0
+fi
+
+#load all the greybus drivers
+gbmods="/system/lib/modules/gb-*"
+for mod in $gbmods
+do
+    insmod $mod
+done
+
+#Load the V4l2 driver for the required for the cameramod.
+insmod /system/lib/modules/v4l2-hal.ko
+start mods_camd #initialize the Camera mod service.
