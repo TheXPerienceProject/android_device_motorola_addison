0fcec871888cd3fe59aaaba9b07ad539e95ff0d1
klozz <klozz707@gmail.com>
2018-02-09 03:16:01 -0500

    addison: fix the issue related to read greybus modules also add the option to enable gbmodinstall in on property:sys.boot_completed=1 to avoid any issue related to not initiate if you pass these others props
    Signed-off-by: klozz <klozz707@gmail.com>

--- rootdir/etc/init.gbmods.sh
+++ rootdir/etc/init.gbmods.sh
@@ -18,36 +18,27 @@ if [ "$debug" == "1" ]; then
 fi
 
 #Load the Greybus main driver 
-insmod /system/vendor/lib/modules/greybus.ko
-
+insmod /vendor/lib/modules/greybus.ko
 
 #Load the Mods Main Driver
-insmod /system/vendor/lib/modules/gb-mods.ko
+insmod /vendor/lib/modules/gb-mods.ko
 
 # Only support PTP and BATTERY in charge-only mode
 if [ "$bootmode" == "charger" ]; then
-    insmod /system/vendor/lib/modules/gb-mods.ko
-    insmod /system/vendor/lib/modules/gb-battery.ko
-    insmod /system/vendor/lib/modules/gb-ptp.ko
+    insmod /vendor/lib/modules/gb-mods.ko
+    insmod /vendor/lib/modules/gb-battery.ko
+    insmod /vendor/lib/modules/gb-ptp.ko
 
     return 0
 fi
 
 #load all the greybus drivers
-gbmods="/system/lib/modules/gb-*"
+gbmods="/vendor/lib/modules/gb-*"
 for mod in $gbmods
 do
     insmod $mod
 done
 
-
-#load all the greybus drivers from vendor
-gbmodsvendor="/system/vendor/lib/modules/gb-*"
-for modvendor in $gbmodsvendor
-do
-    insmod $modvendor
-done
-
 #Load the V4l2 driver for the required for the cameramod.
-insmod /system/vendor/lib/modules/v4l2-hal.ko
+insmod /vendor/lib/modules/v4l2-hal.ko
 start mods_camd #initialize the Camera mod service.
