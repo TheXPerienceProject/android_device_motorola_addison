e40a8011260f9bd928292fd0abcdc769d7465122
klozz <klozz707@gmail.com>
2018-02-09 04:56:45 -0500

    addison: allow the system to build all the camera HAL from default
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -100,12 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libbson
-
-# Camera HIDL HAL
-PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
+    libbson \
     android.hardware.camera.provider@2.4-impl
 
 PRODUCT_COPY_FILES += \
