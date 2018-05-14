589ce9cb2d6c0d54efe1e60752249e20c668c66b
klozz <klozz707@gmail.com>
2018-05-14 00:06:40 -0400

    addison: Add missing rild props and changes this may fix dual sim issues
    Signed-off-by: klozz <klozz707@gmail.com>

--- rootdir/etc/init.mmi.boot.sh
+++ rootdir/etc/init.mmi.boot.sh
@@ -15,9 +15,20 @@ notice()
 start_copying_prebuilt_qcril_db()
 {
     if [ -f /vendor/radio/qcril_database/qcril.db -a ! -f /data/vendor/radio/qcril.db ]; then
-        cp /vendor/radio/qcril_database/qcril.db /data/vendor/radio/qcril.db
+        # [MOTO] - First copy db from the old N path to O path for upgrade
+        if [ -f /data/misc/radio/qcril.db ]; then
+            cp /data/misc/radio/qcril.db /data/vendor/radio/qcril.db
+            # copy the backup db from the old N path to O path for upgrade
+            if [ -f /data/misc/radio/qcril_backup.db ]; then
+                cp /data/misc/radio/qcril_backup.db /data/vendor/radio/qcril_backup.db
+            fi
+            # Now delete the old folder
+            rm -fr /data/misc/radio
+        else
+            cp /vendor/radio/qcril_database/qcril.db /data/vendor/radio/qcril.db
+        fi
         chown -h radio.radio /data/vendor/radio/qcril.db
-    else
+     else
         # [MOTO] if qcril.db's owner is not radio (e.g. root),
         # reset it for the recovery
         qcril_db_owner=`stat -c %U /data/vendor/radio/qcril.db`
@@ -66,7 +77,29 @@ fi
 #
 # Copy qcril.db if needed for RIL
 #
-tart_copying_prebuilt_qcril_db
+start_copying_prebuilt_qcril_db
 echo 1 > /data/vendor/radio/db_check_done
 
+#
+# Make modem config folder and copy firmware config to that folder for RIL
+#
+if [ -f /data/vendor/radio/ver_info.txt ]; then
+    prev_version_info=`cat /data/vendor/radio/ver_info.txt`
+else
+    prev_version_info=""
+fi
 
+cur_version_info=`cat /firmware/verinfo/ver_info.txt`
+if [ ! -f /firmware/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
+    rm -rf /data/vendor/radio/modem_config
+    mkdir /data/vendor/radio/modem_config
+    chmod 770 /data/vendor/radio/modem_config
+    cp -r /firmware/image/modem_pr/mcfg/configs/* /data/vendor/radio/modem_config
+    chown -hR radio.radio /data/vendor/radio/modem_config
+    cp /firmware/verinfo/ver_info.txt /data/vendor/radio/ver_info.txt
+    chown radio.radio /data/vendor/radio/ver_info.txt
+fi
+ 
+cp /firmware/image/modem_pr/mbn_ota.txt /data/vendor/radio/modem_config
+chown radio.radio /data/vendor/radio/modem_config/mbn_ota.txt
+echo 1 > /data/vendor/radio/copy_complete
