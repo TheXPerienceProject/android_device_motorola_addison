b095bf32fa73799c5f20fd9b3288dbb31fbad155
klozz <shaclone707@gmail.com>
2018-07-30 02:57:17 -0400

    addison: fix typo Dx
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -275,7 +275,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Power HAL
-PRODUCT_COPY_FILES += \
+PRODUCT_PACKAGES += \
      android.hardware.power@1.0-service.addison
 
 # Perf configuration
