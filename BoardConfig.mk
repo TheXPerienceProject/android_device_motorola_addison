64045ae56433168f1751b022dc5b000c5bda989b
klozz <shaclone707@gmail.com>
2018-08-02 22:35:46 -0400

    Addison: Add a dsp symlink to vendor/lib
    Signed-off-by: klozz <shaclone707@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,7 @@ TARGET_RIL_VARIANT := caf
 
 # Root
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
+BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
 
 #SDCLANG
 TARGET_USE_SDCLANG := true
