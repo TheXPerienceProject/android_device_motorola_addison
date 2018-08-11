322e22fbebd8d8f631be3712d39bfde3d19f098a
Bruno Martins <bgcngm@gmail.com>
2018-08-11 07:47:33 -0400

    addison: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
     * Support for vendor.prop was abandoned
    
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The XPerience Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
