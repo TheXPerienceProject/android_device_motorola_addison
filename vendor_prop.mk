5665379c32840ffe671fd9a2fb31c94c5f502633
klozz <shaclone707@gmail.com>
2018-09-19 03:15:27 -0400

    addison: update bluetooth vendor
    Signed-off-by: klozz <shaclone707@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -50,7 +50,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.hfp.client=1 \
-    qcom.bluetooth.soc=smd \
+    vendor.qcom.bluetooth.soc=smd \
     qcom.bt.le_dev_pwr_class=1 \
     ro.bluetooth.dun=true \
     ro.bluetooth.hfp.ver=1.7 \
