c0d310b16d304daf1673d2b2344da1660ba472a5
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2018-11-24 06:52:09 -0600

    Addison: Add skeleton of Health 2.0 service
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- device.mk
+++ device.mk
@@ -205,8 +205,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
    android.hardware.health@1.0-impl\
    android.hardware.health@1.0-convert \
-   android.hardware.health@2.0-impl \
-   android.hardware.health@1.0-service
+   android.hardware.health@1.0-service.Addison
 
 # IMS
 PRODUCT_PACKAGES += \
