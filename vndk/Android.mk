6b9f90b18d429a319a9875b9e0839d11159dfeb0
klozz <shaclone707@gmail.com>
2018-12-04 12:57:16 -0500

    addison: fix vndk
    Signed-off-by: klozz <shaclone707@gmail.com>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -34,7 +34,15 @@ VNDK_SP_LIBRARIES := \
     libz
 
 EXTRA_VENDOR_LIBRARIES := \
-    android.hardware.nfc@1.0
+    android.hardware.nfc@1.0 \
+    android.hidl.memory@1.0 \
+    android.hidl.base@1.0 \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.2 \
+    vendor.display.config@1.3 \
+    vendor.display.config@1.4 \
+    vendor.xperience.power@1.0
 
 #-------------------------------------------------------------------------------
 # VNDK Modules
@@ -54,6 +62,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $3
+LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -68,6 +77,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $3
+LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
 endif  # TARGET_TRANSLATE_2ND_ARCH is not true
 endif  # TARGET_2ND_ARCH is not empty
