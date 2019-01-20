956c4080bf8b5851d1737a2be85a5f13077b98bb
klozz <shaclone707@gmail.com>
2019-01-20 03:38:20 -0500

    addison:drop ipacm
    Signed-off-by: klozz <shaclone707@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,9 +183,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Release Tools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
-#Ril
-TARGET_RIL_VARIANT := caf
-
 # Root
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
