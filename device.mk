b16fea5217ff742b58fc2d2c3bcbc437c2f218f9
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2019-04-25 21:09:02 -0500

    addison: update thermal from deen PPK29.68-16-21
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- device.mk
+++ device.mk
@@ -386,7 +386,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
-    thermal.msm8953 \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
