43d944a9feaacb704edb0bcb7add88a1ab2bc873
klozz <klozz@vivaldi.net>
2019-05-30 12:23:44 +0000

    addison: dont forget to track the fking shim
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -367,6 +367,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
+    libshim_ril \
     libsensorndkbridge \
     librmnetctl \
     libqdMetaData.system \
