af06ea48f7ab5ef98195ad71a4f28b4bdb20c73a
klozz <klozz@vivaldi.net>
2019-06-15 23:51:06 +0000

    addison: initial Test ThermalController
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -391,7 +391,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    ThermalController
 
 # USB
 PRODUCT_PACKAGES += \
