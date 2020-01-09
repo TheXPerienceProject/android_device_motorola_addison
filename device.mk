524cdb0bf2064beea882233e3e7dcf7ef997e61e
klozz <klozz@vivaldi.net>
2020-01-09 22:16:24 +0000

    addison: use libprocessgroup for audio hal Our old audio lib have some changes to work on Q so enable it to fix build on Q Yes seems we need old lib to fix audio issue on addison :/
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -83,6 +83,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     audiod \
