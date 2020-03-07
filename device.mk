a0c0ad58fd85ff0e8993341d15623cdc716b8fdf
klozz <klozz@vivaldi.net>
2020-03-07 02:52:27 +0000

    addison: move drm to 1.0 move sound trigger to 2.1
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -84,9 +84,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \
     android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8953 \
