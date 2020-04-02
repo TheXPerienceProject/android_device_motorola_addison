d80a82d44aa1d8dd4d7aad24e3e3abf20f6d8dfc
klozz <klozz@vivaldi.net>
2020-04-02 06:13:18 +0000

    addison: build System As  System this avoid to build SAR system image
    Signed-off-by: klozz <klozz@vivaldi.net>

--- configs/post_install.sh
+++ configs/post_install.sh
@@ -3,6 +3,6 @@
 variant=`getprop ro.boot.radio`
 
 if [ $variant == "China" ]; then
-    rm /system/vendor/etc/libnfc-nxp.conf
-    mv /system/vendor/etc/libnfc-nxp_ds.conf /system/vendor/etc/libnfc-nxp.conf
+    rm /vendor/etc/libnfc-nxp.conf
+    mv /vendor/etc/libnfc-nxp_ds.conf /vendor/etc/libnfc-nxp.conf
 fi
