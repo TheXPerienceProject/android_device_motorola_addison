377a5ffd02cc43ca33255f1cf14597ff4a07c066
klozz <carlosj@klozz.dev>
2022-04-06 10:02:30 +0000

    Revert "addison: Drop telephony duplications"
    This reverts commit 5d72a4bf5f3518cff68fb3e03aea05ff392039c5.

--- device.mk
+++ device.mk
@@ -415,6 +415,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-addison.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
